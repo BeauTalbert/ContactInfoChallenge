a296f5f7c94a5621973d45610c6c670fe561f2aa
BeauTalbert <37456604+BeauTalbert@users.noreply.github.com>
2018-03-16 16:38:58 -0500

    Add files via upload

---
+++ ContactInfoVersion3/ContactInfoVersion3/ContactInfoVersion3/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "ContactInfo.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	ContactInfo contactIn;
+	contactIn.getContactInfo(argv[1]);
+	//print out the information
+	cout << "Name: " << contactIn.getName() << endl;
+	cout << "Phone: " << contactIn.getPhoneNumber() << endl;
+	cout << "Email: " << contactIn.getEmailAddress() << endl;
+
+
+	//system("pause");
+	return 0;
+}
